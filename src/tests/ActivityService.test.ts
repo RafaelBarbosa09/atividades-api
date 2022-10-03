e34aee62c236134973b54ead1daaeae42b1558c2
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2022-10-03 00:43:18 -0300

    adicionando test cases

---
+++ src/tests/ActivityService.test.ts
@@ -0,0 +1,47 @@
+// save activity test case
+
+import ActivityRepository from "../repositories/impl/ActivityRepository";
+import ActivityService from "../services/ActivityService";
+
+const activity = {
+    id: null,
+    idStudent: "1",
+    idCourse: "1",
+    deadline: new Date(),
+    question: "Qual a capital do Brasil?",
+} as Activity;
+
+describe("ActivityService", () => {
+    it("should be able to save an activity", async () => {
+        const activityRepository = new ActivityRepository();
+        const activityService = new ActivityService(activityRepository);
+
+        const activitySaved = await activityService.createActivity(activity);
+        expect(activitySaved.id).not.toBeNull();
+    });
+
+    it("should be able to update an activity", async () => {
+        const activityRepository = new ActivityRepository();
+        const activityService = new ActivityService(activityRepository);
+
+        const activitySaved = await activityService.createActivity(activity);
+
+        activitySaved.question = "Qual a capital da Inglaterra?";
+        const activityToBeUpdated = {
+            ...activitySaved,
+        } as Activity;
+
+        const activityUpdated = await activityService.update(activitySaved);
+        expect(activityToBeUpdated).toEqual(activityUpdated);
+    });
+
+    it("should be able to submit an activity", async () => {
+        const activityRepository = new ActivityRepository();
+        const activityService = new ActivityService(activityRepository);
+
+        const activitySaved = await activityService.createActivity(activity);
+        const activitySubmited = await activityService.submitActivity(activitySaved.id as string, "Brasília");
+
+        expect(activitySubmited?.id).not.toBeNull();
+    });
+});
