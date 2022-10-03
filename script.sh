6e7deff773e6945c2aa453f4544c3f04d5ae3291
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2022-10-03 20:09:43 -0300

    acerta host do rabbitmq

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ echo "executando rabbitmq"
 docker run -d --hostname my-rabbit  --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 
 echo "subindo container"
-docker run -p 3000:3000 atividades-api
+docker run --network=host atividades-api
